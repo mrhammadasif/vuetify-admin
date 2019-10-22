032431d5edc03763c2e26cd3e9284fa17c110093
Hammad Asif <mrhammadasif@gmail.com>
2019-10-22 13:34:05 +0500

    completed first services page

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -5,87 +5,91 @@
       <div
         slot="header"
         class="flex items-center justify-between">
-        <h3>Connections</h3>
+        <h3>Services</h3>
         <div class="flex justify-end">
           <vs-button
             icon="refresh"
             color="warning"
             type="filled"
             class="mr-2"
-            @click.stop="loadConnections()"
+            @click.stop="reloadData()"
           >Refresh</vs-button>
-          <vs-button
+          <!-- <vs-button
             id="connections-step1"
             icon="link"
             color="primary"
             type="filled"
             @click.stop="$router.push({name: 'app-connections-add'})"
-          >Add Connection</vs-button>
+          >Add Connection</vs-button> -->
         </div>
       </div>
       <div>
         <vs-table :data="connections">
           <template slot="thead">
             <vs-th>
-              Connector Type
+              Name
             </vs-th>
             <vs-th>
-              Connection Name
+              Contract Type
             </vs-th>
             <vs-th>
-              Client Key
+              Industry
             </vs-th>
             <vs-th>
-              Version
-            </vs-th>
-            <vs-th>
-              Year
+              Service Type
             </vs-th>
             <vs-th>
               Created
             </vs-th>
             <vs-th>
-              Status
+              Actions
             </vs-th>
           </template>
 
           <template slot-scope="{data}">
             <vs-tr
-              v-for="(tr, indextr) in data"
-              :key="indextr" >
+              v-for="tr in data"
+              :key="tr._id" >
 
-              <vs-td class="pl-8">
+              <!-- <vs-td class="pl-8">
                 <img
-                  :src="basePath + data[indextr].providerIconUrl"
+                  :src="basePath + tr.name"
                   height="32"
                   alt="">
+              </vs-td> -->
+
+              <vs-td :data="tr.name">
+                <span v-html="tr.name"></span><br>
+                <span class="text-small">{{ tr._id }}</span>
               </vs-td>
 
-              <vs-td :data="data[indextr].connectionName">
-                {{ data[indextr].connectionName }}<br>
-                <span class="text-small">{{ data[indextr].apiResourcesUrl }}</span>
+              <vs-td :data="tr.contractType">
+                {{ startCase(tr.contractType) }}
               </vs-td>
 
-              <vs-td :data="data[indextr].apiKey">
-                {{ data[indextr].apiKey }}
+              <vs-td :data="tr.scope.industry">
+                {{ startCase(tr.scope.industry ) }}
               </vs-td>
 
-              <vs-td :data="data[indextr].apiVersion">
-                {{ data[indextr].apiVersion }}
-              </vs-td>
-              <vs-td :data="data[indextr].apiYear">
-                {{ data[indextr].apiYear }}
+              <vs-td :data="tr.scope.serviceType">
+                {{ startCase(tr.scope.serviceType) }}
               </vs-td>
 
-              <vs-td :data="data[indextr].createdDate">
-                <vx-tooltip :text="data[indextr].createdDate | formatDate">
-                  {{ data[indextr].createdDate | elapsedTime }}
+              <vs-td :data="tr.createdAt">
+                <vx-tooltip :text="tr.createdAt | formatDate">
+                  {{ tr.createdAt | elapsedTime }}
                 </vx-tooltip>
               </vs-td>
-
-              <vs-td :data="data[indextr].statusDisplayName">
-                <app-status-badge :status="data[indextr].statusDisplayName" />
+              <vs-td>
+                <vs-button
+                  type="filled"
+                  icon="delete"
+                  color="danger"
+                  @click="deleteService(tr._id)"/>
               </vs-td>
+              <!-- <vs-td :data="tr.statusDisplayName">
+                <app-status-badge :status="tr.statusDisplayName" />
+              </vs-td> -->
             </vs-tr>
           </template>
         </vs-table>
@@ -99,6 +103,7 @@
 import Vue from "vue"
 import axios from "axios"
 import apiConfig from "@/../api_config.json"
+import {startCase} from "lodash"
 
 export default Vue.extend({
   data: () => {
@@ -106,10 +111,39 @@ export default Vue.extend({
   },
   computed: {basePath: () => apiConfig.EXTERNAL_ASSETS_BASEURL},
   mounted () {
-    // this.loadConnections()
+    this.reloadData()
   },
   methods: {
-    async loadConnections () {
+    startCase,
+    deleteService (serviceId) {
+      this.$vs.dialog({
+        type: "confirm",
+        color: "danger",
+        title: "Are you sure",
+        text: `You want to delete the service with Id: ${serviceId}`,
+        accept: () => this.acceptConfirm(serviceId)
+      })
+    },
+    async acceptConfirm (serviceId) {
+      try {
+        await axios.delete("/services/" + serviceId)
+        this.$vs.notify({
+          color: "danger",
+          title: "Deleted",
+          text: "The selected service was successfully deleted"
+        })
+        this.reloadData()
+      }
+      catch (e) {
+        this.$vs.dialog({
+          type: "alert",
+          color: "danger",
+          title: "Failed to delete",
+          text: "Reason: " + (e.response.data || e.message)
+        })
+      }
+    },
+    async reloadData () {
       this.$vs.loading({text: "Loading Services ..."})
       const {data} = await axios.get("/services")
       this.connections = data
