9412aa9f4e49b8a8c8e837ced52cc5c57ea2c6e4
Hammad Asif <mrhammadasif@gmail.com>
2019-10-23 17:21:09 +0500

    done with overview page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <vs-card>
-      <div>
-        <h3 class="m-0 p-0">Welcome to ISV Admin Panel</h3>
+      <div class="flex items-center">
+        <h3 class="m-0 flex-grow p-0">Welcome to ISV Admin Panel</h3>
+        <vs-button
+          icon="refresh"
+          @click="refreshData">Reload</vs-button>
       </div>
     </vs-card>
     <div class="flex">
@@ -14,16 +17,16 @@
           <div class="flex justify-between">
             <div class="flex flex-col">
               <span>New Users</span>
-              <h1>31</h1>
+              <h1>{{ recentUsers.length }}</h1>
               <span>In 30 days</span>
             </div>
             <div class="flex flex-col">
               <span>Total Users</span>
-              <h1>200</h1>
+              <h1>{{ users.length }}</h1>
             </div>
             <div class="flex flex-col">
               <span>Pro Users</span>
-              <h1>23</h1>
+              <h1>{{ proUsers.length }}</h1>
             </div>
           </div>
         </vs-card>
@@ -39,15 +42,16 @@
               v-for="tr in recentUsers"
               :key="tr._id"
               class="flex justify-between">
-              <div class="border px-4 py-2">{{ tr.profile.name }} <br> {{ tr.createdAt | formatDate }}</div>
+              <div class="border px-4 py-2">{{ get(tr, "profile.name") }} <br> {{ tr.createdAt | formatDate }}</div>
               <div class="border px-4 py-2">
                 <vs-button
-                  color="primary"
-                  type="filled"
+                  icon="info"
                   @click="$router.push(`/users/edit/${tr._id}`)">
-                  View Details
                 </vs-button>
               </div>
+            </div>
+            <div v-if="recentUsers.length <= 0">
+              <p>No <span class="font-bold text-danger">Recent Users</span> found who registered in previous 30 days</p>
             </div>
 
           </div>
@@ -69,6 +73,8 @@ p,ul{
 
 <script>
 import axios from "axios"
+import {get} from "lodash"
+import moment from "moment"
 export default {
   data () {
     return {
@@ -78,38 +84,48 @@ export default {
     }
   },
   computed: {
+    lastFewUsers () {
+      return this.recentUsers.slice(-2)
+    },
+    proUsers () {
+      return this.users.filter((user) => user.type == "paidUser")
+    },
     recentUsers () {
-      return this.users.slice(-2)
+      return this.users.filter((user) => moment(user.createdAt).isAfter(moment().subtract(30, "days")))
     }
   },
   mounted () {
-    this.$vs.loading({text: "Loading Data..."})
-    Promise.all([
-      this.loadUsers(),
-      this.loadServices(),
-      this.loadProposals()
-    ]).then(() => {
-      this.$vs.loading.close()
-    })
-      .catch((e) => {
-        this.$vs.notify({
-          text: `Unable to fetch data from server. Refresh the page. Reason: ${e.response.data || e.message}`,
-          color: "danger"
-        })
-      })
+    this.refreshData()
   },
   methods: {
+    get,
+    refreshData () {
+      this.$vs.loading({text: "Loading Data..."})
+      Promise.all([
+        this.loadUsers(),
+        this.loadServices(),
+        this.loadProposals()
+      ]).finally(() => {
+        this.$vs.loading.close()
+      })
+        .catch((e) => {
+          this.$vs.notify({
+            text: `Unable to fetch data from server. Refresh the page. Reason: ${e.response.data || e.message}`,
+            color: "danger"
+          })
+        })
+    },
     async loadUsers () {
       const {data} = await axios.get("/users")
       this.users = data
     },
     async loadServices () {
       const {data} = await axios.get("/services")
-      this.users = data
+      this.services = data
     },
     async loadProposals () {
       const {data} = await axios.get("/proposals")
-      this.users = data
+      this.proposals = data
     }
   }
 }
