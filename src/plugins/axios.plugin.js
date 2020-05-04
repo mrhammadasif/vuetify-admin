db183b44a673b2438d862c3373db409ddf1cb801
Hammad <mrhammadasif@gmail.com>
2020-05-05 03:19:08 +0500

    moved headerContentTYpe def to interceptor

--- src/plugins/axios.plugin.js
+++ src/plugins/axios.plugin.js
@@ -4,9 +4,9 @@ import store from "../store/index"
 export default function (config) {
   Axios.interceptors.request.use((request) => {
     request.headers.Authorization = store.getters.authToken
+    request.headers["Content-Type"] = "application/json; charset=utf-8"
     return request
   })
 
   Axios.defaults.baseURL = `${config.API_BASE_URL}`
-  Axios.defaults.headers.common["Content-type"] = "application/json"
 }
